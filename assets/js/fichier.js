88d382a9f54bcd9c4722e8c777d27c4946dff059
massa <massafranck@gmail.com>
2018-06-25 18:30:40 +0200

    corrigé

---
+++ assets/js/fichier.js
@@ -0,0 +1,6 @@
+//Déclaration de la fonction
+function displaytext(){
+  //déclaration et assignation de la variable
+  var lastname = document.getElementById('lastname').value;
+  alert(lastname);
+}
